edc68cebf9998eb3a5570703f53cec58a9a8e41c
Caleb <158685767+CMPrograms@users.noreply.github.com>
2024-10-30 10:54:14 -0400

    submitted work

--- hw6.py
+++ hw6.py
@@ -1,16 +1,23 @@
 import math 
 import random
 
-def get_number(str):
+def get_number():
   user_number = None
   while user_number == None:
     try:
-      user_number = int(input(str))
+      user_number = int(input(f"Enter an integer: "))
       return user_number
     except ValueError:
       print("Invalid Input!")
 
-
+def get_index(list1):
+  while True:
+    try:
+     index = get_number()
+     list1[index] = list1[index]
+     return index
+    except IndexError:
+      print("Invalid Index Number")
 
 def factorize(number): 
   factors = []
@@ -31,34 +38,40 @@ def is_square(number):
     else:
       return False
 
-def increment(number):
-  number += 1
-  return number
+
+def increment(list1):
+  choice = get_index(list1)
+  list1[choice] += 1
+  return list1[choice]
 
 numbers = []
 
 for i in range(20):
   numbers.append(random.randrange(100))
 
-print("The list of numbers is", numbers)
+
 
 
 choice = None
 while choice != "done":
+  print("The list of numbers is", numbers)
   choice = input("Enter a command(factorize/square/quartile/increment/append/done): ").lower()
+
   if choice == "factorize":
     # Print out the prime factors of the number.
-    number = get_number("Enter a number: ")
-    print("The factors of the number are ", factorize(number))
+    factors =  factorize(numbers[get_index(numbers)])
+    print(f"The factors of the number are {factors}")
+
   elif choice == "square":
-    number = get_number("Enter a number: ")
+    number = numbers[get_index(numbers)]
     if is_square(number):
       print("The number is square")
     else:
       print("The number is not square")
+
   elif choice == "quartile":
     # Check what quartile the number fits into. (1-25, 25-50, 50-75, 75-100)
-    x = get_number("Enter a number 1-100: ")
+    x = numbers[get_index(numbers)]
     if x > 100:
       print("Invalid Input")
     elif x >= 75:
@@ -69,11 +82,14 @@ while choice != "done":
       print("The number is in the second quartile")
     else:
       print("The number is in the first quartile")
+
   elif choice == "increment":
     # Increase the value of the number by 1.
-    print(increment(get_number("Enter a number: ")))
+    increment(numbers)
+
   elif choice == "append":
     # Add a new number to the end of the list.
-    numbers.append(get_number("Enter a number to add to the end of the list: "))
+    numbers.append(get_number())
+
   elif choice != "done":
     print("Command not recognized.")
